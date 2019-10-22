5a3ac0afe7ec97ddbe59108ed3c851ccc9569b53
Zabana <karim.cheurfi@gmail.com>
2019-10-22 17:11:42 +0200

    rename common ports constnt and allowed modes list

--- molly/constants.py
+++ molly/constants.py
@@ -1,9 +1,10 @@
 FIRST_1000_PORTS = 1024
 ALL_PORTS = 65535
-COMMON_PORTS = [
+TOP_20_PORTS = [
     21, 22, 23, 25, 53, 80, 110, 
     111, 135, 139, 143, 443, 445, 
     993, 995, 1723, 3306, 3389, 
     5900, 8080
 ]
 VERSION = '0.1.0'
+ALLOWED_MODES = ['basic', 'custom', 'common', 'full']
