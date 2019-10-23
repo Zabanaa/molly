7ade3a97890288f8d56625afab72f3160d56ae42
Zabana <karim.cheurfi@gmail.com>
2019-10-23 16:26:09 +0200

    change generic exception to ValueError in is_ip_v4 helper

--- molly/utils.py
+++ molly/utils.py
@@ -4,11 +4,10 @@ from ipaddress import ip_address
 def is_ip_v4(target):
     try:
         ip_address(target)
-    except Exception:
+    except ValueError:
         return False
     else:
         return True
-
 
 def format_datetime(dt):
     return dt.strftime("%d %B %Y %I:%M:%S %p")
