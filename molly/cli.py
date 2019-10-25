12883c6d0af39adf8fde9aea2c25408b712e5451
Zabana <karim.cheurfi@gmail.com>
2019-10-25 15:37:39 +0200

    added command descriptions

--- molly/cli.py
+++ molly/cli.py
@@ -1,10 +1,22 @@
 from molly import Molly
-from molly.constants import ALLOWED_MODES
+from molly.constants import ALLOWED_MODES, VERSION
 import click
 import sys
 
 @click.group()
 def cli():
+    r"""
+
+    Molly is a network reconaissance tool. 
+    Use it to scan an IP/Host for open ports.
+    
+    /!\ REMINDER /!\
+
+    Scanning ports against targets for which you don't have EXPLICIT WRITTEN PERMISSION
+    from their owners is a may be considered a CRIME depending on your local law. 
+    You should only use this tool on your own hosts/IPs 
+    or those you have been given permission to scan.
+    """
     pass
 
 @cli.command()
@@ -15,11 +27,24 @@ def cli():
                 type=click.Choice(
                     ALLOWED_MODES,
                     case_sensitive=False
-                )
+                ),
+                help="Scan mode"
             )
-@click.option('--workers', default=100, type=int)
+@click.option('--workers', default=100, type=int, help='How many worker threads to run')
 def scan(target, mode, workers):
+    """
+    Perform a TCP port scan against the specified target.
 
+    Allowed modes are: basic, full, custom and common
+    
+    basic: the first 1023 reserved ports.
+
+    full: will perform a full scan on all TCP ports.
+
+    common: scan the top 20 most commonly used ports. This includes ssh, ftp, http, https ...
+
+    custom: will prompt you to enter your desired port range (separated by a comma)
+    """
     molly = Molly(target=target, mode=mode, workers=workers)
 
     try:
