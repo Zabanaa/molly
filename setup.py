83bf81390b48cb3dc0cce9a01e12893f5e614e54
Zabana <karim.cheurfi@gmail.com>
2019-10-26 14:56:22 +0200

    added setup.py configuration

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='molly-py',
+    version='0.1.0',
+    description='A multithreaded TCP port scanner.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(
+        exclude=['docs', 'tests']
+    ),
+    url="https://github.com/Zabanaa/molly",
+    author="Karim C",
+    author_email="karim.cheurfi@gmail.com",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable'
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Security'
+    ],
+    keywords="multithreaded tcp port scanner",
+    python_requires='>=3.7',
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'molly=molly.cli:cli'
+        ]
+    }
+)
